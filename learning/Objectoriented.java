7b8214ca927b1ac3f54ed4d5c7ae27817692bd35
asrar <akifullakhanpc3@gmail.com>
2026-09-04 12:53:44 +0530

    class terminologies

---
+++ learning/Objectoriented.java
@@ -0,0 +1,23 @@
+package learning;
+
+public class Objectoriented {
+    public static void main(String[] args) {
+        //chapter 8
+        //class is blueprint for to create a object instance 
+        //what is object oriented programming
+        /*
+        solving a problem by creating object is one of the most
+        popular approaches in programming this is know as object ortineted programming  
+        */
+       //DRY -- do not repeat yourself
+       
+       // object is a instance of a class 
+       //opps terminologies
+       //abstraction -- hiding the internal details show essential info
+       //encapsulation -- the act of puttin carious component together in a capsule hiding sensitive data
+       //polymorphism -- many forms  
+       //inheritance -- the act of driving new things from existing things
+    }
+}
+
+
