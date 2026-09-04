8eb67252d83b88bf6e97a5bfc1002f3fb373d48e
asrar <akifullakhanpc3@gmail.com>
2026-09-04 13:15:04 +0530

    creating new class

--- learning/Class.java
+++ learning/Class.java
@@ -10,6 +10,24 @@ class NewClass{
     }
 }
 
+class Employee{
+    int id;
+    String name;
+    int sal;
+    void getSalary(){
+        System.out.println(sal);
+    }
+    void getDetails(){
+        System.out.println("name "+name);
+        System.out.println("id "+id);
+        System.out.println("sal "+sal);
+    }
+    void getname(){
+        System.out.println(name);
+    }
+
+}
+
 
 public class Class {
     public static void main(String[] args) {
@@ -17,5 +35,18 @@ public class Class {
         NewClass obj1 = new NewClass(); //createing a new object here
         obj1.add();
 
+        Employee harry = new Employee();
+        Employee akif = new Employee();
+        harry.id =11;
+        harry.name="Harry";
+        harry.sal =350;
+
+        harry.getDetails();
+        System.out.println();
+        akif.id =12;
+        akif.name="Akif";
+        akif.sal =3500;
+
+        akif.getDetails();
     }
 }
